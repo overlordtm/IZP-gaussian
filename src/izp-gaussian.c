0a76a4ffe5750422983fbcb477c21a5e3e7a3b96
Andraz Vrhovec <andraz@vrhovec.si>
2012-03-27 22:13:59 +0200

    Drgac zacet!

---
+++ src/izp-gaussian.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : izp-gaussian.c
+ Author      : Andraz Vrhovec
+ Version     :
+ Copyright   : Use it, abuse it!
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
