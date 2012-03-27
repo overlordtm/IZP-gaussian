88ccdf249dddda9962f515f9c4706d8d2c1e40b5
Andraz Vrhovec <andraz@vrhovec.si>
2012-03-27 22:31:11 +0200

    Pizdarija!

--- src/izp-gaussian.c
+++ src/izp-gaussian.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!Jestsemzakon"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
